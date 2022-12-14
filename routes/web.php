78304c79e2e8011e8ce51146deb7ac57e5a0023c
Oni Restu Pamuji <oni.pamuji@spesolution.net>
2022-12-15 01:11:03 +0700

    update test v3

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::get('/admin/pelanggan', function () {
 });
 
 Route::get('/test', function () {
-    return 'test v3';
+    return 'test v3 - oni';
 });
 
 Route::get('/bootstrap', function () {
